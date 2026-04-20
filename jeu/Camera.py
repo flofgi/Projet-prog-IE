b53ef4ccb1019026848e4b26a31b350d049da0ab
Barynonyx <simon.deulniau@gmail.com>
2026-04-20 14:10:29 +0200

    Zoom methods added with associated modifications in the existing methods, nomenclature modifications

--- jeu/Camera.py
+++ jeu/Camera.py
@@ -1,26 +1,53 @@
 import pygame
+import Map
+from Player import Player
+
 
 class Camera :
-  def __init__(self, mapsize, screensize, tilesize = (32,32)):
+  def __init__(self, mapsize, windowsize, tilesize = (32,32), coefscale=1.0):
     """initialize the camera who follows the player
     Args:
+      window(py.display): the window of the game
       mapsize(tuple): the size of the map in tiles
-      screensize(tuple): the size of the screen in pixel
+      windowsize(tuple): the size of the window in pixel
       tilesize(tuple): the size of a unique tile in pixel, 32 by 32 by default
+      scale(int): zoom coefficient
     """
     self.mapsize = mapsize
-    self.screensize = screensize   
+    self.windowsize = windowsize   
     self.tilesize = tilesize
+
+    self.scaled_window = pygame.Surface(self.windowsize)
+    self.max_scale = 2
+    self.min_scale = 0.04
+    self.coefscale = coefscale
 
     self.x = 0
     self.y = 0
-    self.max_x = mapsize[1] * tilesize[0] - screensize[0]
-    self.max_y = mapsize[0] * tilesize[1] - screensize[1]
+    self.max_x = mapsize[1] * tilesize[0] - windowsize[0]
+    self.max_y = mapsize[0] * tilesize[1] - windowsize[1]
 
   def update(self, player):
     """update the position of the camera following the player's coordinates and the border of the map"""
-    self.x = player.getPosition()[0] - self.screensize[0]//2
-    self.y = player.getPosition()[1] - self.screensize[1]//2
+    #self.x = player.getPosition[0] - self.screensize[0]//(2*self.coefscale)
+    #self.y = player.getPosition[1] - self.screensize[1]//(2*self.coefscale)
+    self.x = player[0] - self.scaled_window.get_width()// 2
+    self.y = player[1] - self.scaled_window.get_height()// 2
     self.x = max(0, min(self.x, self.max_x))
     self.y = max(0, min(self.y, self.max_y))
+  
+  def scaling(self, newscale):
+    """update the zoom of the window depending on the scale gived, and redraw the map"""   
+    self.coefscale = max(self.min_scale, min(self.max_scale, newscale))
+    zoom = (int(self.windowsize[0] * self.coefscale), int(self.windowsize[1] * self.coefscale))
+    self.scaled_window = pygame.Surface(zoom)
+    #return self.scaled_window.get_size()
+
+  def render(self, map_obj: Map, window):
+    self.scaled_window.fill((0,0,50))
+    map_obj.draw(self)
+
+    scaled = pygame.transform.scale(self.scaled_window, self.windowsize)
+    window.blit(scaled,(0,0))
+  
   
