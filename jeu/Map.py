866d2d57e3b919f86f0f911f1a998016f8a58993
Barynonyx <simon.deulniau@gmail.com>
2026-04-23 16:23:00 +0200

    Map and Tileset file separation, personnal testing file added

--- jeu/Map.py
+++ jeu/Map.py
@@ -14,7 +14,7 @@ import numpy as np
 from Player import Player
 from WorldElement import WorldElement
 from events import RECUP_EVENT, ALLY_EVENT
-
+from Tileset import Tileset
 
 
 class Map :
@@ -80,35 +80,3 @@ class Map :
         for element in self.worldelements:
             element.load()
 
-class Tileset:
-    def __init__(self, file, tilesize : tuple, margin=1, spacing=1):
-      self.file = file
-      self.tilesize = tilesize
-      self.margin = margin
-      self.spacing = spacing
-      self.image = pygame.image.load(file)
-      self.tiles = []
-      self.load()
-
-    @property
-    def getTileSize(self):
-        return self.tilesize
-
-    def load(self):
-      """cut the image containing the tiles into individual tiles in a list"""
-      self.tiles = []
-      x0 = y0 = self.margin
-      w,h = self.image.get_size()
-      dx = self.tilesize[0] + self.spacing
-      dy = self.tilesize[1] + self.spacing
-      
-      for x in range(x0, w, dx):
-          for y in range(y0, h, dy):
-              tile = pygame.Surface(self.tilesize)
-              tile.blit(self.image, (0, 0), (x, y, *self.tilesize))
-              self.tiles.append(tile)
-
-    def unload(self):
-      """Nullify the tile list"""
-      for i in range (len(self.tiles)):
-        self.tiles[i]=None
